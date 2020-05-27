16f044f6e0a8ac5652ab2697b75e761c9a6d4c3b
Bi <15734069790@163.com>
2020-05-27 17:31:01 +0800

    remove something

--- api/modules/v1/controllers/ApiController.php
+++ api/modules/v1/controllers/ApiController.php
@@ -230,7 +230,7 @@ class ApiController extends BaseController
     {
         $page = \Yii::$app->request->post('page') ?? 1;
 
-        $xinggan = Tmp::find()->limit(10)->groupBy('name')->orderBy('RAND()')->all();
+        $xinggan = Tmp::find()->limit(10)->orderBy('RAND()')->all();
 
         $data['data'] = $xinggan;
         return ApiResponse::success($data);
@@ -240,7 +240,7 @@ class ApiController extends BaseController
     {
         $page = \Yii::$app->request->post('page') ?? 1;
 
-        $qingchun =  Tmp::find()->limit(10)->groupBy('name')->orderBy('RAND()')->all();
+        $qingchun =  Tmp::find()->limit(10)->orderBy('RAND()')->all();
         if (empty($qingchun)){
             $qingchun = Tmp::find()->offset(($page-8)*10)->limit(10)->groupBy('name')->orderBy('id desc')->all();
         }
