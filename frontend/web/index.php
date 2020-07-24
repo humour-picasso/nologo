2817471841b35659064cc37f4a16db3f20658aeb
Bi <15734069790@163.com>
2020-07-24 14:46:09 +0800

    去水印网站登陆部分

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,6 +1,29 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'prod');
+$prod_List = [
+    "www.littlecloud.fun"
+];
+
+$dev_List = [
+    'nologo.local.com',
+];
+
+
+if (in_array($_SERVER['HTTP_HOST'], $prod_List)) {
+
+    error_reporting(0);
+    ini_set('display_errors', 1);
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
+} elseif (in_array($_SERVER['HTTP_HOST'], $dev_List)) {
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+
+} else {
+    exit('domain miss');
+}
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
@@ -13,5 +36,4 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../config/main.php',
     require __DIR__ . '/../config/main-local.php'
 );
-
 (new yii\web\Application($config))->run();
