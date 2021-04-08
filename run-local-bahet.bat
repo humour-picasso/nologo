3467b3c261a1c3725093a61796a3522cbef01cc1
jiaxing <15734069790@163.com>
2021-04-08 11:03:10 +0800

    Update run-local-bahet.bat

--- run-local-bahet.bat
+++ run-local-bahet.bat
@@ -1,10 +1,10 @@
 @echo off
-if exist [".\.env" ] (
-  set %@% = grep -v '^#' .env | xargs -0
+if exist .\.env (
+  set /P OEM=<.env
 ) else (
   echo "please copy the .env.tpl file to .env file."
-  exit 1
+  exit
 )
-.\bin\behat --colors "%@%"
+.\bin\behat --colors %OEM%
 php -S 0.0.0.0:8000
-echo "To view HTML report visit: http:/\localhost:8000\build\report"
+echo "To view HTML report visit: http://localhost:8000/build/report"
