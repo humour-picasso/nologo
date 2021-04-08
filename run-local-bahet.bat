869919987308da6afee24c6596bf02b0d31c30a9
jiaxing <15734069790@163.com>
2021-04-08 14:11:30 +0800

    Update run-local-bahet.bat

--- run-local-bahet.bat
+++ run-local-bahet.bat
@@ -1,10 +1,27 @@
 @echo off
 if exist .\.env (
-  set /P OEM=<.env
+	setlocal enabledelayedexpansion
+	for /f %%i in (.env) do (
+		set %%i
+		)
 ) else (
-  echo "please copy the .env.tpl file to .env file."
-  exit
+  	echo "please copy the .env.tpl file to .env file."
+  	exit
 )
-.\bin\behat --colors %OEM%
+
+set var=
+:LOOP
+	set index=%1
+	if %index%! == ! goto END
+	
+rem	add your logic in here
+	call set "var=%%var%% %%index%%"
+	shift
+	goto LOOP
+	
+:END
+
+
+.\bin\behat --colors %var% 
 php -S 0.0.0.0:8000
-echo "To view HTML report visit: http://localhost:8000/build/report"
+@echo "To view HTML report visit: http://localhost:8000/build/report"
